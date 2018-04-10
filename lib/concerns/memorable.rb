8cc81cb37f0eb778ecc688fefb91b578dbb6c793
Luis Rodriguez <zacha@tutanota.com>
2018-04-10 02:50:20 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,19 @@
-module Findable
+module Memorable
+  
   module ClassMethods
-    
-    def find_by_name(name)
-      self.all.detect{|o| o.name}
-    end    
-    
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
+  
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
+  
 end
